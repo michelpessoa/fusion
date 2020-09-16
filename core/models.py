2b49cefd577eeee1eb57471c012103ffeef9682c
Michel Pessoa Keursten <michelpessoa@gmail.com>
2020-09-16 08:10:25 -0300

    Fix media save

--- core/models.py
+++ core/models.py
@@ -1,6 +1,12 @@
+import uuid
 from django.db import models
 
 from stdimage.models import StdImageField
+
+def get_file_path(_instance, filename):
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+    return filename
 
 
 class Base(models.Model):
@@ -48,7 +54,7 @@ class Funcionario(Base):
     nome = models.CharField('Nome', max_length=100)
     cargo = models.ForeignKey('core.Cargo', verbose_name='Cargo', on_delete=models.CASCADE)
     bio = models.TextField('Bio', max_length=200)
-    imagem = StdImageField('Imagem', upload_to='equipe',
+    imagem = StdImageField('Imagem', upload_to=get_file_path,
                            variations={'thumb': {'width': 480, 'height': 480, 'crop': True}})
     facebook = models.CharField('Facebook', max_length=100, default='#')
     twitter = models.CharField('Twitter', max_length=100, default='#')
