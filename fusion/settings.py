ecb6dee8b75dea8b4f986943086ec6a7164cbcd4
Michel Pessoa Keursten <michelpessoa@gmail.com>
2020-09-17 09:57:41 -0300

    Study finished

--- fusion/settings.py
+++ fusion/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qdd%j0=fl((21&o9a(xk-o$vpi*iq2_+m97ykx6_mmh^6k7_u5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
